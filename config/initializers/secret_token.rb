391867f60c2f50e27e1a34fc4ba6c254b9b250c7
Se Hoon Park <sheep@sehoonpark.com.ar>
2013-01-18 13:09:27 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRecurlySubscriptionSaas::Application.config.secret_token = 'e37602c5328ccdaa38b672d282d820c2db1fee7e3b4c76487757e36e6656e234ec6dfc28cb3f267158c8835c03fb35b917d37ae4f41155fbb0da3421e516bb66'
